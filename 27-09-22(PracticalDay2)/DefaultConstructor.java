21b01612951fed4252d5f171ac8066a0b1e93cc1
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-27 12:54:51 +0530

    Add files via upload

---
+++ 27-09-22(PracticalDay2)/DefaultConstructor.java
@@ -0,0 +1,31 @@
+package Practical;
+
+//public class DefaultConstructor {
+//  int age;  // instance variable default value is zero
+//  String name;  // instance variable default value is null
+//  void show() {
+//	  System.out.println(age+ "" +name);
+//	  
+//  }
+//  public static void main(String[] args) {
+//	DefaultConstructor obj1=new DefaultConstructor();
+//	obj1.show();
+//}
+//}
+
+public class DefaultConstructor {
+	  int age;  // instance variable default value is zero
+	  String name;  // instance variable default value is null
+	  DefaultConstructor() {
+		  System.out.println("Default constructor no need to called ");
+		  System.out.println(age+ "" +name);
+		  
+	  }
+	  public static void main(String[] args) {
+		DefaultConstructor obj1=new DefaultConstructor();
+		
+	}
+	}
+
+
+
